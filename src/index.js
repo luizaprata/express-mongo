421bfc76c3dc7f87912cfe4b0814ad89a6a27f98
Luiza Prata <luiza.prata@lambda3.com.br>
2024-05-26 19:45:29 -0300

    bootstrap

---
+++ src/index.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+var app = require('./app.js');
+var debug = require('debug')('app');
+
+var port = process.env.PORT || '3000';
+
+app.on('error', onError);
+app.on('listening', onListening);
+app.listen(port);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+
+  console.log('Listening on ' + bind);
+  debug('Listening on ' + bind);
+}
+
